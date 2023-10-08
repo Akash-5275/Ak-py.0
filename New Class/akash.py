9f1e2714125312fac760178353e88ae5354745a7
Akash-5275 <mdatik5275hasan@gmail.com>
2023-10-08 22:49:39 +0600

    first commit

---
+++ New Class/akash.py
@@ -0,0 +1,39 @@
+
+#calculator making with python
+
+a= int(input("inter the first value\n"))
+b= int(input("inter the second value\n"))
+
+#Addition
+c= a+b
+print("The Addition is", c)
+
+#Subtraction
+d= a-b
+print("The Subtraction is", d)
+
+#Multiplication
+e= a*b
+print("The Multiplication is", e)
+
+#Division
+f= a/b
+print("The Division is", f)
+
+#Modulus
+g= a%b
+print("The Modulus is",g)
+
+
+#Floor Division
+h= a//b
+print("The Floor Division is",h)
+
+
+echo "# Ak-py.0" >> README.md
+
+
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Akash-5275/Ak-py.0.git
+git push -u origin main
